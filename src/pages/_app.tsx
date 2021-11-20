366a4f074aed9099dd7f5d7e0ecc771c3c845038
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-20 15:28:11 +0000

    Update：プロジェクト作成＆ESLintとPrettier設定

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import 'styles/globals.css'
+import type { AppProps } from 'next/app'
+import React from 'react'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+export default MyApp
