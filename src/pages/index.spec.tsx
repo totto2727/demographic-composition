792f876f7792c4168b680c015c52a2d4f3ac252b
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-20 17:17:53 +0000

    Add：テスト環境の構築
    テスト用ファイルとテストするファイルは基本同じ階層へ置く

---
+++ src/pages/index.spec.tsx
@@ -0,0 +1,10 @@
+import { squere } from './index'
+
+describe('index.tsx Function Testcase', () => {
+  it('should return the squared value', () => {
+    const result = squere(2)
+    const expected = 4
+
+    expect(result).toStrictEqual(expected)
+  })
+})
