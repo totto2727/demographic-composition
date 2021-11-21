4ede5a383b872b297ab28d1131ac132d0e545309
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-21 05:58:59 +0000

    Add：RESAS APIの都道府県一覧リクエストの型情報追加

--- src/utils/resas/index.ts
+++ src/utils/resas/index.ts
@@ -1,2 +1,15 @@
-// TODO APIリクエスト実装
-export {}
+export type Prefecture = {
+  prefCode: number
+  prefName: string
+}
+
+export type PrefecturesResponse = {
+  message?: string
+  result: Prefecture[]
+}
+
+export type APIFailResponse = {
+  statusCode: string
+  message: string
+  description: string
+}
