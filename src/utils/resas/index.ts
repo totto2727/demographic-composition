0b5741a25ee5df55a54de0fa7f87cab1dbf8ae68
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-21 08:12:24 +0000

    Add：APIに都道府県一覧をリクエストする関数を追加
    ・RESASの仕様に合わせて一部の型を修正
    ・RESASにリクエストするためのAxios Instance生成

--- src/utils/resas/index.ts
+++ src/utils/resas/index.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosError } from 'axios'
+
 export type Prefecture = {
   prefCode: number
   prefName: string
@@ -8,8 +10,35 @@ export type PrefecturesResponse = {
   result: Prefecture[]
 }
 
-export type APIFailResponse = {
-  statusCode: string
-  message: string
-  description: string
+export type APIFailResponse =
+  | {
+      statusCode: string
+      message?: string
+      description?: string
+    }
+  | string
+  | undefined
+
+const RESAS_ENDPOINT = 'https://opendata.resas-portal.go.jp'
+
+const resasAxiosInstanse = axios.create({
+  baseURL: RESAS_ENDPOINT,
+  headers: {
+    'X-API-KEY': process.env.RESAS_API_KRY || '',
+  },
+})
+
+export const getPrefectures: () => Promise<
+  PrefecturesResponse | APIFailResponse
+> = async () => {
+  const path = 'api/v1/prefectures'
+  try {
+    const response = await resasAxiosInstanse.get<PrefecturesResponse>(path)
+    return response.data
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      return (e as AxiosError<APIFailResponse>).response?.data
+    }
+    return undefined
+  }
 }
