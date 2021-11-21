91bc3676390b2f80854bc35b9abff98c3d92dd0d
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-21 02:30:57 +0000

    開発の準備
    ・Emotion追加
    ・ファイルの整理

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
-import 'styles/globals.css'
+
+import './_app.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
--- src/pages/index.spec.tsx
+++ src/pages/index.spec.tsx
@@ -1,10 +1,10 @@
-import { squere } from './index'
+// import { squere } from './index'
 
-describe('index.tsx Function Testcase', () => {
-  it('should return the squared value', () => {
-    const result = squere(2)
-    const expected = 4
+// describe('index.tsx Function Testcase', () => {
+//   it('should return the squared value', () => {
+//     const result = squere(2)
+//     const expected = 4
 
-    expect(result).toStrictEqual(expected)
-  })
-})
+//     expect(result).toStrictEqual(expected)
+//   })
+// })
