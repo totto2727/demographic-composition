770013af12a60b238d63f73767be7e6e1b51681c
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-23 18:02:06 +0000

    Improve:APIリクエスト関連のテスト改善
    ・テストで複数回使うオブジェクトを個別のファイルに移動
    ・型ガードのテストに429エラーのときを追加

---
+++ src/utils/resas/__mock__/resas403ErrorRawResponse.ts
@@ -0,0 +1,9 @@
+import { RESASErrorRawResponse } from '../types'
+
+const RESAS_403_ERROR_RAW_RESPONSE: RESASErrorRawResponse = {
+  statusCode: '403',
+  message: 'Forbidden.',
+  description: '',
+}
+
+export default RESAS_403_ERROR_RAW_RESPONSE
---
+++ src/utils/resas/__mock__/resas429ErrorRawResponse.ts
@@ -0,0 +1,9 @@
+import { RESASErrorRawResponse } from '../types'
+
+const RESAS_429_ERROR_RAW_RESPONSE: RESASErrorRawResponse = {
+  statusCode: '403',
+  message: 'Forbidden.',
+  description: '',
+}
+
+export default RESAS_429_ERROR_RAW_RESPONSE
