bde6d1082c61c15c2bd7e9fa17c00bc51484a825
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-24 03:29:03 +0000

    Add:RESAS APIの人口に関するレスポンスの型定義を追加
    実際にリクエストした際のJsonをテスト用のデータとして追加した。

--- src/utils/resas/types.ts
+++ src/utils/resas/types.ts
@@ -113,3 +113,19 @@ export type Prefecture = {
   prefCode: number
   prefName: string
 }
+
+export type PopulationCompositionItem = {
+  year: number
+  value: number
+  rate?: number
+}
+
+export type PopulationCompositionItems = {
+  label: '総人口' | '年少人口' | '生産年齢人口' | '老年人口'
+  data: PopulationCompositionItem[]
+}
+
+export type PopulationCompositionPerYear = {
+  boundaryYear: number
+  data: PopulationCompositionItems[]
+}
