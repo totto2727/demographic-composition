98ba976606459e6c94a8324a8f4da7ee271fa142
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-30 23:03:36 +0000

    Remove:不要なインポートを削除

--- src/components/prefecture-checkboxes/index.tsx
+++ src/components/prefecture-checkboxes/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback } from 'react'
 import useCheckedPrefecture from 'utils/hooks/useCheckedPrefectures'
 import { Prefecture } from 'utils/resas/types'
 
